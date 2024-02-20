b969419195581fe8286d93e6592aff9eae7dbcbe
Zitro11 <israelortiz2001@gmail.com>
2024-02-19 20:34:58 -0600

    Base1

--- doggoapp/test/widget_test.dart
+++ doggoapp/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:doggoapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DoggoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
