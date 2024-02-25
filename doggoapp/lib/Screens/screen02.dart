45ceb3b90f3b94ab0ff2fd9a53de4eaf8acdba7b
Zitro11 <israelortiz2001@gmail.com>
2024-02-25 15:13:56 -0600

    Api Google Maps

--- doggoapp/lib/Screens/screen02.dart
+++ doggoapp/lib/Screens/screen02.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
-class Mapa extends StatelessWidget {
+class Mapa extends StatefulWidget {
   const Mapa({super.key});
+
+  @override
+  State<Mapa> createState() => _MapaState();
+}
+
+class _MapaState extends State<Mapa> {
+  late GoogleMapController mapController;
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +20,15 @@ class Mapa extends StatelessWidget {
         backgroundColor: Colors.indigo,
       ),
 
-      body: const Center(
-        child: Text("Hola")
+      body:  GoogleMap(
+        onMapCreated: (controller) {
+          mapController = controller;
+        },
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(37.7749, -122.4194), // Coordenadas de San Francisco
+          zoom: 12.0,
+        ),
       ),
-
     );
   }
 }
