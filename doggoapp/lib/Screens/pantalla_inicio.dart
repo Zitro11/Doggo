bec782fc0c067fb11c96e6bd8da13e06327ff5d9
Zitro11 <israelortiz2001@gmail.com>
2024-03-10 19:56:42 -0500

    ScrollView

--- doggoapp/lib/Screens/pantalla_inicio.dart
+++ doggoapp/lib/Screens/pantalla_inicio.dart
@@ -60,116 +60,113 @@ class _PantallaInicioState extends State<PantallaInicio> with TickerProviderStat
       drawer: const BarraLateral(), //Barra lateral
 
       body:
-        Stack(  //Stack para interponer widgets
-          children: [ //un array de widgets
-            /*Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(urlImages[indexMonumento]),
-                  fit: BoxFit.cover, 
-                  //colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.5), BlendMode.dstATop)
-                )
-              ),
-            ),
-            BackdropFilter( //Hacer borrosa la imagen de fondo
-              filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
-              child: Container(
-                color: Colors.transparent,
-              ),
-            ),*/
-            Row(    //un renglon para poner el primer texto
-              mainAxisAlignment: MainAxisAlignment.start,
+        SingleChildScrollView(  //Widget que permite dezplasar la pantalla cuando no cabe todo
+          child:
+            Column(
               children: [
-                Container(
-                  margin: const EdgeInsets.all(12.0),
-                  child: const Text(
-                    'Una vision al mundo',
-                    style: TextStyle(fontFamily: 'Roboto', fontSize: 20.0, color: Colors.brown),
-                  ),
-                )
-              ],
-            ),
-            Column( //Una columna con un renglon para poder alinear el nombre del monumento
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                Stack(  //Stack para interponer widgets
+                  children: [ //un array de widgets
+
+                Row(    //un renglon para poner el primer texto
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Container(
-                      margin: const EdgeInsets.only(top: 90.0),
-                      //decoration: const BoxDecoration(color: Colors.blue),  //llamamos del objeto el titulo dependiendo la posicion en el array con el index
-                      child: Text(elementos[indexMonumento].titulo, style: const TextStyle(fontFamily: '', fontSize: 20.0, color: Colors.black),),
+                      //height: 40,
+                      margin: const EdgeInsets.all(12.0),
+                      child: const Text(
+                        'Una vision al mundo...',
+                        style: TextStyle(fontFamily: 'Roboto', fontSize: 20.0, color: Colors.brown),
+                      ),
+                    )
+                  ],
+                ),
+                Column( //Una columna con un renglon para poder alinear el nombre del monumento
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(top: 80.0),
+                          //decoration: const BoxDecoration(color: Colors.blue),  //llamamos del objeto el titulo dependiendo la posicion en el array con el index
+                          child: Text(elementos[indexMonumento].titulo, style: const TextStyle(fontFamily: '', fontSize: 20.0, color: Colors.black),),
+                        ),
+                      ],
                     ),
+                  ],
+                ),
+                Column(     //La columna para posicionar el carouselSlider
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 120), // Caja para separar
+                    CarouselSlider.builder(
+                        itemCount: elementos.length,  //le decimos la longitud con la cantidad de objetos
+                        itemBuilder: (context, index, realIndex){
+                          final urlImage = elementos[index].urlImage;   //le especificamos la imagen que se la trae dependiendo el index
+                          return GestureDetector(
+                            onTap: () {
+                              _animationController.forward().then((_) {
+                                // Detener la animación justo antes de navegar a la nueva pantalla
+                                _animationController.stop();
+
+                                // Esperar un breve momento antes de navegar
+                                Future.delayed(const Duration(milliseconds: 500), () {
+                                  // Navegar a la siguiente pantalla después de que la animación haya terminado
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) => elementos[indexMonumento].screen),
+                                  );
+                                  
+                                });
+                                // Reiniciar la animación para la próxima vez
+                                  _animationController.reset();
+                              });
+                            },
+                            child: AnimatedBuilder(
+                              animation: _animationController,
+                              builder: (context, child) {
+                                return Transform.scale(
+                                  scale: _animation.value,
+                                  child: BuildImageWidget(urlImage: urlImage, index: index),
+                                );
+                              },
+                            ),
+                          );
+                        },
+                        options: CarouselOptions(
+                          height: 380,
+                          onPageChanged: (index, reason) =>
+                            setState(()=>indexMonumento = index),
+                        ),
+                      ),
+                    const SizedBox(height: 12), // Caja para separar                                    // Llamamos al widget indicador visual de la posición de la imagen
+                    BuildIndicator(indexMonumento: indexMonumento, urlImagesLength: longitudElementos), //Le mandamos el index para q sepa en cual esta y el tama;o para la cantidad
+                  ]
+                ),
+                Column(
+                  children: [    //Texto de abajo (descripcion)
+                    const SizedBox(height: 550),
+                    Container(
+                    
+                    //color: Colors.blue.withOpacity(0.5), // Fondo semitransparente
+                    padding: const EdgeInsets.symmetric(horizontal: 30.0),  //separacion del texto con respecto a las orillas
+                    margin: const EdgeInsets.only(bottom: 10.0),   //separacion del container de abajo
+                    constraints: const BoxConstraints(maxHeight: 130),
+                     // Altura máxima del contenedor
+                    child: SingleChildScrollView(
+                      child: Text(
+                        elementos[indexMonumento].descripcion,  //le damos la descripcion del objeto seleccionado
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 15.0, color: Colors.black),
+                      ),
+                    ),
+                  ),
                   ],
                 ),
               ],
             ),
-            Column(     //La columna para posicionar el carouselSlider
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //const SizedBox(height: 12), // Caja para separar
-                CarouselSlider.builder(
-                    itemCount: elementos.length,  //le decimos la longitud con la cantidad de objetos
-                    itemBuilder: (context, index, realIndex){
-                      final urlImage = elementos[index].urlImage;   //le especificamos la imagen que se la trae dependiendo el index
-                      return GestureDetector(
-                        onTap: () {
-                          _animationController.forward().then((_) {
-                            // Detener la animación justo antes de navegar a la nueva pantalla
-                            _animationController.stop();
-
-                            // Esperar un breve momento antes de navegar
-                            Future.delayed(const Duration(milliseconds: 500), () {
-                              // Navegar a la siguiente pantalla después de que la animación haya terminado
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => elementos[indexMonumento].screen),
-                              );
-                              
-                            });
-                            // Reiniciar la animación para la próxima vez
-                              _animationController.reset();
-                          });
-                        },
-                        child: AnimatedBuilder(
-                          animation: _animationController,
-                          builder: (context, child) {
-                            return Transform.scale(
-                              scale: _animation.value,
-                              child: BuildImageWidget(urlImage: urlImage, index: index),
-                            );
-                          },
-                        ),
-                      );
-                    },
-                    options: CarouselOptions(
-                      height: 380,
-                      onPageChanged: (index, reason) =>
-                        setState(()=>indexMonumento = index),
-                    ),
-                  ),
-                const SizedBox(height: 12), // Caja para separar                                    // Llamamos al widget indicador visual de la posición de la imagen
-                BuildIndicator(indexMonumento: indexMonumento, urlImagesLength: longitudElementos), //Le mandamos el index para q sepa en cual esta y el tama;o para la cantidad
               ]
-            ),
-            Align(    //Texto de abajo (descripcion)
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                //color: Colors.blue.withOpacity(0.5), // Fondo semitransparente
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),  //separacion del texto con respecto a las orillas
-                margin: const EdgeInsets.only(bottom: 10.0),   //separacion del container de abajo
-                constraints: const BoxConstraints(maxHeight: 130), // Altura máxima del contenedor
-                child: SingleChildScrollView(
-                  child: Text(
-                    elementos[indexMonumento].descripcion,  //le damos la descripcion del objeto seleccionado
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: 15.0, color: Colors.black),
-                  ),
-                ),
-              ),
-            ),
-          ],
+            )
         ),
-
     );
   }
 }
